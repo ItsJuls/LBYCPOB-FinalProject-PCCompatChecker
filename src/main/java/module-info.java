cd0ec58b67c5e8339c618b4442bcf877d8ad803c
ItsJules <itsjulesyt@gmail.com>
2026-08-11 17:50:38 +0800

    Open package for FXML access in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.pccompatchecker {
     requires javafx.controls;
     requires com.fasterxml.jackson.annotation;
 
+    opens com.pccompatchecker to javafx.fxml;
+
     exports com.pccompatchecker;
 }
 
