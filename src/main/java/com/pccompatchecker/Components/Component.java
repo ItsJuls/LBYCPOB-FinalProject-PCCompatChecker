a239dd74f7f7bad3ae2d2618e8466f4741c929f4
ItsJules <itsjulesyt@gmail.com>
2026-08-11 10:43:46 +0800

    Add abstract Component class to represent hardware components

---
+++ src/main/java/com/pccompatchecker/Components/Component.java
@@ -0,0 +1,34 @@
+package com.pccompatchecker.Components;
+
+import java.util.Optional;
+
+public abstract class Component {
+    private String name;
+    private Optional<Double> price;
+    private String brand;
+
+    public Component(String name, Double price){
+        this.name = name;
+        this.price = Optional.ofNullable(price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Optional<Double> getPrice() {
+        return price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    abstract String getCategory();
+    abstract String getSpecSummary();
+
+    @Override
+    public String toString() {
+        return name + " - $" + price;
+    }
+}
