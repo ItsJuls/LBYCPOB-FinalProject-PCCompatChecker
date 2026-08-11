ad58fb6b1ab50641be4a470190fd4b09f025bb0f
ItsJules <itsjulesyt@gmail.com>
2026-08-11 17:51:13 +0800

    Add FXML support in module-info.java for view integration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.pccompatchecker {
     requires javafx.controls;
     requires com.fasterxml.jackson.annotation;
+    requires javafx.fxml;
 
     opens com.pccompatchecker to javafx.fxml;
 
